ec39fde5f61418303e9096cefb2a625d62278eac
Maksim Laptenko <laptenko0811@icloud.com>
2021-04-07 15:31:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 7389;
+        int bonusMilli = ticket / 20;
+        System.out.println("За покупку билета вам начисленно " + bonusMilli + " бонусных миль");
+    }
+}
